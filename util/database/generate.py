1dab02d509361f7db0974095b873fcc9357b9eb3
JoaoVictorSou <soulima.joao@gmail.com>
2023-05-05 11:42:20 -0300

    feat: database generator

---
+++ util/database/generate.py
@@ -0,0 +1,114 @@
+import sqlite3
+import os
+
+DIRECTORY_PATH = os.path.abspath('.')
+CONNECT_STRING = os.path.join(
+    DIRECTORY_PATH,
+    'data',
+    'hotel.db'
+)
+
+hotels = [
+    {
+    'hotel_id': 'alpha',
+    'name': 'Alpha Hotel',
+    'star': 4.3,
+    'price': 420.34,
+    'city': 'Rio de Janeiro',
+    },
+    {
+    'hotel_id': 'bravo',
+    'name': 'Bravo Hotel',
+    'star': 4.4,
+    'price': 380.90,
+    'city': 'Santa Catarina',
+    },
+    {
+    'hotel_id': 'charlie',
+    'name': 'Charlie Hotel',
+    'star': 3.9,
+    'price': 320.2,
+    'city': 'Santa Catarina',
+    },
+]
+hotels = [tuple(hotel.values()) for hotel in hotels]
+
+def create_hotel_table():
+    try:
+        connection = sqlite3.connect(CONNECT_STRING)
+        cursor = connection.cursor()
+
+        sql_create_hotel_table = """
+            CREATE TABLE IF NOT EXISTS hotels (
+                hotel_id text PRIMARY KEY,
+                name text,
+                star real,
+                price real,
+                city text
+            )
+        """
+        cursor.execute(sql_create_hotel_table)
+        
+    except Exception as err:
+        print(f'ERR: {err}')
+
+    finally:
+        cursor.close()
+        connection.close()
+
+def insert_hotel(hotels: list):
+    # Só vai criar a tabela no caso que ela não exista.
+    create_hotel_table()
+
+    sql_insert = """
+        INSERT INTO HOTELS (
+            hotel_id,
+            name,
+            star,
+            price,
+            city
+        ) VALUES (
+            ?,
+            ?,
+            ?,
+            ?,
+            ?
+        )
+    """
+
+    try:
+        connection = sqlite3.connect(CONNECT_STRING)
+        cursor = connection.cursor()
+
+        cursor.executemany(
+            sql_insert,
+            hotels
+        )
+
+        connection.commit()
+
+    finally:
+        cursor.close()
+        connection.close()
+
+def select_all_hotels():
+    all_hotels = []
+    
+    try:
+        connection = sqlite3.connect(CONNECT_STRING)
+        cursor = connection.cursor()
+
+        sql_query = """
+            SELECT *
+            FROM hotels
+        """
+
+        cursor.execute(sql_query)
+
+        all_hotels = cursor.fetchall()
+    finally:
+        cursor.close()
+        connection.close()
+    
+    return all_hotels
+
